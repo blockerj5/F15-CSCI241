107a0c76da6e50e8c47076fa479ea02a66406777
blockerj5 <blockerj5@mailbox.winthrop.edu>
2015-11-17 20:51:06 -0500

    Update vending.php

--- lab5/vending.php
+++ lab5/vending.php
@@ -1,5 +1,4 @@
-
-		<form method="post" action="vending.php">
+<form method="post" action="vending.php">
 			Total Bill: <input type="integer" name="totalBill"></input><br/>
 			Total Tendered: <input type="integer" name="totalTendered"></input><br/>
 			
@@ -9,15 +8,12 @@
 
 
 <?php
-
 if(isset($_POST['submit']))
  {
 $totalBill =$_POST['totalBill'];
 $totalTendered =$_POST['totalTendered'];
-
 $change=$totalTendered-$totalBill;
 $currency = array(100,50,20,10,5,1,0.25,0.10,0.05,0.01);
-
 echo "<h1>Receipt</h1>";
 echo "Total Bill: ".$totalBill;
 echo"</br>";
@@ -25,33 +21,30 @@ echo "Total Tendered: ".$totalTendered;
 echo"</br>";
 echo "Change: ".$change;
 echo"</br>";
-
 echo "<table border=1";
 foreach($currency as $nextcurrency)
 {
- $currentcurrency=$nextcurrency;
- if(($change/$currentcurrency)>=1)
+ $currentCurrency=$nextcurrency;
+ if(($change/$currentCurrency)>=1)
  {
- 
- $BillQuantity=floor($change/$currentcurrency);
- 
- $disbursed=$BillQuantity*$currentcurrency;
+ $disbursed=currency($change, $currentCurrency, $billQuantity);
  $change=$change-$disbursed;
  } 
  else {
-  $BillQuantity=0;
+  $billQuantity=0;
   $disbursed=0;
  }
  echo "<tr>";
  echo "<td>".$nextcurrency."</td>";
- echo "<td>".$BillQuantity."</td>";
+ echo "<td>".$billQuantity."</td>";
  echo "<td>".$disbursed."</td>";
  echo "</tr>";
 }
 echo "</table>";
-
-
-
     
  }   
+ function currency($change, $currentCurrency, &$billQuantity){
+ $billQuantity=floor($change/$currentCurrency);
+ return $billQuantity*$currentCurrency;
+ }
 ?>
