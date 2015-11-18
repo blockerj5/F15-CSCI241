3baf1b532b3f590d45fd50793dd49d5262ccaae1
blockerj5 <blockerj5@mailbox.winthrop.edu>
2015-11-17 20:01:35 -0500

    Update lab4.php

--- lab4/lab4.php
+++ lab4/lab4.php
@@ -3,14 +3,14 @@
  	<body>
 
  	
- <form name="table" id="table" action="lab4.php" method="post">
+ <form name="table" id="table" action="test.php" method="post">
       <h1>Multiplication Table Creator</h1>
       <p>
-    <label for="width">Start Number:</label>
+    <label for="width">Start:</label>
     <input type="number" name="width" id="width" />
       </p>
       <p>
-    <label for="height">End number:</label>
+    <label for="height">End:</label>
     <input type="number" name="height" id="height" />
       </p>
     <input type="submit" value="submit" name="submit">
@@ -24,50 +24,41 @@
  if(isset($_POST['submit']))
  {
  	$start =(int) $_POST['width'];
-	// 
+	if(!is_int($start)){
+		echo"Start number is not valid.";
+		exit(1);
+	}
  	$end = (int) $_POST['height'];  
-	//
-	
+	if(!is_int($end)){
+		echo"End number is not valid.";
+		exit(1);
+	}
 	echo "<table border='1'>";
   //Encompass this data printed out in a table
-    for($r=0; $r<$end; $r++)
+  	
+    for($j=$start; $j<=$end; $j++)
     {
-       	echo "<tr>";
-		echo "<td>" . $r . "</td>";
+    	echo "<tr>";
+		if($j==$start){
+			echo "<td>" . " " . "</td>";
+			for($i=$start; $i<=$end ; $i++)
+        		{ echo "<td>" . ($i) . "</td>";}
+				echo"</tr>";
+				echo"<tr>";
+				echo "<td>" . $j . "</td>";
+				for($i=$start; $i<=$end ; $i++)
+        		{echo "<td>" . ($j * $i) . "</td>";}}
+		else{echo "<td>" . $j . "</td>";      
+			for($i=$start; $i<=$end ; $i++)
+        		{echo "<td>" . ($j * $i) . "</td>";}
+		    }
 		
-       for($c=1; $c<$start ; $c++)
-        {
-        	if($r==0)
-			{
-				echo "<td>" . $c. "</td>";
-			}
-			else {
-				 echo "<td>" . ($r * $c) . "</td>";
-				
-			}
-          //Creates columns
-         
-		  
-        }
 		echo '</tr>';
-		
-          
     }
  echo '</table>'; 
-
  }
- 	
-	
-	
-	
-   
-
-	
-  
- 
 		
 ?>
 
 </body>
-
 </html>
