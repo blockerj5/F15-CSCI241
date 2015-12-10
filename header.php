ac1c2b7619e854e8d8fb952f13920bb3fe1ec4c9
blockerj5 <blockerj5@mailbox.winthrop.edu>
2015-12-10 00:03:45 -0500

    Create header.php

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Project</title>
+		<link rel="stylesheet" type="text/css" href="eggs.css">
+	</head>
+	<div id="content">
+		<div id="nav">
+			 <ul>
+				  <li><a href="index.php">Home</a></li>
+				  <li><a href="about.php">More Information</a></li>
+				  <li><a href="quiz.php">Quiz</a></li>
+			</ul> 
+	</div>
+	<body>
