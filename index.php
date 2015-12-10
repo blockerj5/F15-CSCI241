98428035e69f11a7c3d216eabbf1212b2083dcfd
blockerj5 <blockerj5@mailbox.winthrop.edu>
2015-12-10 00:03:02 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require_once("header.php");
+?>
+<h1>What egg are you?</h1>
+
+<h4>Welcome to my website, where you will be able to find out which egg you are!</h4>
+
+<?php
+require("footer.php");
+?>
