b7249e269e6ac5d6ec4241c9bf3f8061af8cd27e
blockerj5 <blockerj5@mailbox.winthrop.edu>
2015-12-15 08:44:28 -0500

    Update Question1.php

--- Question1.php
+++ Question1.php
@@ -6,7 +6,7 @@ $cookieContent[] = "Is in the way of my break!";
 $finalExamCookie = implode("|", $cookieContent);
 
 header("Set-Cookie: finalexam=" . $finalExamCookie);
-
+//Both of the strings show up in the finalexamcookie and both are divided by the |
 //Cookie has no expire date,but I guess thats fine.
 
 ?>
